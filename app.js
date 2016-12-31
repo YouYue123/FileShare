308dd5089c6793cd5c24bec7fa32a64e8651247c
YouYue <youyue@YueYous-MacBook-Pro.local>
2016-12-31 13:50:34 +0800

    [Feature]-Voice Authen 50%

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-var express = require('express');
-var cfenv = require('cfenv');
+var express = require('express')
+var cfenv = require('cfenv')
 var bodyParser = require('body-parser')
-// create a new express server
-var app = express();
+var fs = require('fs')
+var rs = new require('stream').Readable();
+var app = express()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
+var voiceAuthen = require('./helpers/voiceAuthen.js')
 var socketList = []
-app.set('socket-io',io);
-// parse application/x-www-form-urlencoded
+app.set('socket-io',io)
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
 app.use(bodyParser.json())
+//voiceAuthen.voiceToText(fs.createReadStream('./uploads/sample.wav'))
+io.on('connection',socket=>{
+  console.log(socket.id + ' connected')
+  socket.on('audioChunk', (data) => rs.push(data))
+})
 
 app.all('*', function(req, res, next) {
-       res.header("Access-Control-Allow-Origin", "*");
-       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-       next();
+       res.header("Access-Control-Allow-Origin", "*")
+       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+       next()
 });
-app.use(express.static(__dirname + '/client'));
-app.use(express.static(__dirname + '/uploads'));
-app.use(require('./controllers'));
-
+app.use(express.static(__dirname + '/client'))
+app.use(express.static(__dirname + '/uploads'))
+app.use(require('./controllers'))
 // get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+var appEnv = cfenv.getAppEnv()
 // start server on the specified port and binding host
 server.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
-  console.log(appEnv.name + " server starting on " + appEnv.url);
-});
+  console.log(appEnv.name + " server starting on " + appEnv.url)
+})
--- client/src/components/Authen/index.js
+++ client/src/components/Authen/index.js
@@ -3,9 +3,10 @@ export default class Authen extends Component {
   render(){
     return(
       <div className="auth-mask">
-        <div className="voice-btn">
+        <div className="voice-btn btn btn-success" onClick= {this.props.recordVoice}>
           Voice
         </div>
+        <audio />
       </div>
     )
   }
